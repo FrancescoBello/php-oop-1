1551ab1254140f5b16fb9bbf373a09d2bb7d8a31
Francesco <fraancesco.bello@gmail.com>
2021-01-15 18:19:35 +0100

    fine

--- index.php
+++ index.php
@@ -4,11 +4,14 @@ class Movie{
   public $titolo;
   public $uscita;
   public $trama;
+  public $genere;
 
-  function __construct($_titolo, $_uscita, $_trama){
+  function __construct($_titolo, $_uscita, $_trama, $_genere){
     $this->titolo = $_titolo;
     $this->uscita = $_uscita;
     $this->trama = $_trama;
+    $this->genere = $_genere;
+
   }
   public function getTitle(){
     return $this->titolo;
@@ -16,25 +19,30 @@ class Movie{
   public function getDate(){
     return $this->uscita;
   }
+  public function getDescription(){
+    return $this->trama;
+  }
+  public function getGenre(){
+    return $this->genere;
+  }
 }
 
-  $film = new Movie(" Il Traditore");
-  echo $film->getTitle();
 
 
-  $film->uscita = " 24 ottobre 2019" ;
-  echo $film->getDate();
-  $film->trama = " Le vicende del criminale Tommaso Buscetta, primo pentito di mafia,
-                   che consentì ai giudici Falcone e Borsellino di comprendere l'organizzazione
-                   di Cosa Nostra e di portarne i capi in tribunale.";
-  echo $film->trama;
-  $film2= new Movie(" Prova a Prendermi");
-  echo $film2->getTitle();
-  $film2->uscita = " 31 Gennaio 2003";
-  echo $film2->getDate();
-  $film2->trama = " Un agente dell'FBI è sulle tracce di un giovane artista del travestimento,
+  $film = new Movie(" Il Traditore",  " 24 ottobre 2019"," Le vicende del criminale Tommaso Buscetta, primo pentito di mafia,
+                di Cosa Nostra e di portarne i capi in tribunale.", "Giallo/Drammatico");
+
+  echo $film->getTitle(). "<br>";
+  echo $film->getDate(). "<br>";
+  echo $film->getDescription() . "<br>";
+  echo $film->getGenre() . "<br>";
+  $film2= new Movie(" Prova a Prendermi", " 31 Gennaio 2003", " Un agente dell'FBI è sulle tracce di un giovane artista del travestimento,
                     che è riuscito ad estorcere più di sei milioni di dollari in varie frodi,
-                    impersonando di volta in volta un personaggio diverso.";
+                    impersonando di volta in volta un personaggio diverso.", "Giallo/Drammatico");
+  echo $film2->getTitle(). "<br>";
+  echo $film2->getDate(). "<br>";
+  echo $film2->getDescription(). "<br>";
+  echo $film->getGenre() . "<br>";
 
 
  ?>
